cc5aba3caf04aa9d59fcacd151c654947b452674
Lim <40200760+tbnsok40@users.noreply.github.com>
2020-05-09 13:03:15 +0900

    Add files via upload

--- BAEKJOON/[baek] 1157.py
+++ BAEKJOON/[baek] 1157.py
@@ -1,6 +1,32 @@
-letter = sorted(input().upper())
-editted_letter = letter + ['.']
+# letter = sorted(input().upper())
+# editted_letter = letter + ['.']
+#
+# tmp, sum = None, 1
+# bag = {}
+# for elements in editted_letter:
+#     if tmp==None:
+#         tmp = elements
+#         continue
+#
+#     elif tmp == elements:
+#         sum += 1
+#
+#     else:
+#         bag.update({tmp:sum})
+#
+#         tmp = elements
+#         sum = 1
+#
+# print(bag)
+# def f1(x):
+#     return bag[x]
+# key_max = max(bag.keys(), key=f1)
+# print(key_max)
+# max_key = (max(bag.keys()))
+# print(max_key)
 
+editted_letter = sorted(input().upper()) + ['.']
+# editted_letter = letter + ['.']
 tmp, sum = None, 1
 bag = {}
 for elements in editted_letter:
@@ -16,13 +42,13 @@ for elements in editted_letter:
 
         tmp = elements
         sum = 1
+bag_list = []
 
-print(bag)
-def f1(x):
-    return bag[x]
-key_max = max(bag.keys(), key=f1)
-print(key_max)
-max_key = (max(bag.keys()))
-print(max_key)
+for key, value in bag.items():
+    if value == max(bag.values()):
+        bag_list.append(key)
 
-lambda x: bag.keys(), print()
+if len(bag_list)>1:
+    print('?')
+else:
+    print(bag_list[0])
