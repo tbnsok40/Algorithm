43f687e755ef3867d37f4b646e52ded9be26d92d
Lim <40200760+tbnsok40@users.noreply.github.com>
2020-05-09 12:19:35 +0900

    Add files via upload

--- BAEKJOON/[baek] 1157.py
+++ BAEKJOON/[baek] 1157.py
@@ -1,7 +1,28 @@
 letter = sorted(input().upper())
+editted_letter = letter + ['.']
 
-# if len(letter) ==1:
-#     print(letter[0])
-# else:
-#     print(letter)
+tmp, sum = None, 1
+bag = {}
+for elements in editted_letter:
+    if tmp==None:
+        tmp = elements
+        continue
 
+    elif tmp == elements:
+        sum += 1
+
+    else:
+        bag.update({tmp:sum})
+
+        tmp = elements
+        sum = 1
+
+print(bag)
+def f1(x):
+    return bag[x]
+key_max = max(bag.keys(), key=f1)
+print(key_max)
+max_key = (max(bag.keys()))
+print(max_key)
+
+lambda x: bag.keys(), print()
