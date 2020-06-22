d6093af6694e29a043ab587afe8673909673b54c
Lim <40200760+tbnsok40@users.noreply.github.com>
2020-06-22 20:59:41 +0900

    Add files via upload
    stack의 두번째 인자 path를 지웠음

--- BAEKJOON/algorithm DFS_deepening.py
+++ BAEKJOON/algorithm DFS_deepening.py
@@ -8,25 +8,25 @@ T = Tree(4, Tree(5, Tree(4, Tree(5))), Tree(6, Tree(1), Tree(6)))
 
 def solution(T):
     distinct = {1: set([])}
-    stack = [(T, [T], set([T.x]))] # 말 그대로 스택, [T]는 리스트...이건 왜 필요하지?
+    stack = [(T, set([T.x]))] # 말 그대로 스택,
     i = 1  # number of path
     while stack:  # DFS
-        tree, path, value = stack.pop()
-        if (tree.l == None) and (tree.r == None):  # leaf node
+        node, value = stack.pop()
+        if (node.l == None) and (node.r == None):  # leaf node
             # 이 조건은, 자식 노드가 아무것도 없는 노드에 해당
 
             print('i: ', i, 'value: ', value)
             distinct[i] = value # value는 set으로 중복된 값이 없는 집합이다.
             i += 1 # 즉, dfs 에서 하나의 깊이를 다 팠으니, 다른 깊이(가지)로 넘어간다는 뜻
         else:
-            if tree.r != None:
-                print('tree.r.x:',set([tree.r.x]))
-                stack.append((tree.r, path + [tree.r], value | set([tree.r.x])))
+            if node.r != None:
+                print('node.r.x:',set([node.r.x]))
+                stack.append((node.r, value | set([node.r.x])))
 
             # 아래 조건이, elif 나 else 가 아닌 이유는 곧, 위의 조건문과 상관없다는 뜻
-            if tree.l != None:
-                print('tree.l.x: ',set([tree.l.x]))
-                stack.append((tree.l, path + [tree.l], value | set([tree.l.x])))
+            if node.l != None:
+                print('node.l.x: ',set([node.l.x]))
+                stack.append((node.l, value | set([node.l.x])))
 
     answer = 1
     for key in distinct.keys():
@@ -39,4 +39,7 @@ def solution(T):
 print(solution(T))
 
 # # stack=[(1,3,4)]
-# # print(type(stack[0])) # this is Tuple, 이 스택은 튜플을 저장한다. 그 튜플에는 tree, path, value가 포함돼있다.
+# # print(type(stack[0])) # this is Tuple, 이 스택은 튜플을 저장한다. 그 튜플에는 node, path, value가 포함돼있다.
+
+
+# ref: https://cyc1am3n.github.io/2019/04/26/bfs_dfs_with_python.html
